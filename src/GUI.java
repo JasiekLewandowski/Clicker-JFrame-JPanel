f3c99c638fc5dd6e8080255b9502d4fbcb0d54a9
jasiek_lewandowski <lewandowskifilm@gmail.com>
2020-09-07 14:25:07 +0100

    Initial commit

---
+++ src/GUI.java
@@ -0,0 +1,38 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GUI implements ActionListener {
+    int count = 0;
+    JLabel label;
+
+
+    public GUI() {
+        JFrame frame = new JFrame("Clicker");
+
+        JPanel panel = new JPanel();
+
+        JButton button = new JButton("CLICK ME");
+        button.addActionListener(this);
+
+        label = new JLabel(("Number of clicks: 0"));
+
+        panel.setBorder(BorderFactory.createEmptyBorder(30,30,10,30));
+        panel.setLayout(new GridLayout(0,1));
+        panel.add(button);
+        panel.add(label);
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        //frame.setSize(400,400);
+        frame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        count++;
+        label.setText("Number of clicks " + count);
+    }
+}
